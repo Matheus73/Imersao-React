f2522b944d4dec1f3610e0eac08da651dd8822a0
matheus gabriel <matheuscoding@gmail.com>
2020-07-29 11:45:47 -0300

    changed the content of videos

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
+        videoDescription={"Jovens que queriam ser os donos do mundo mas acabaram descobrindo quem é o verdadeiro dono do mundo e decidem empunhar a espada em seu nome."}
         />
 
         <Carousel
