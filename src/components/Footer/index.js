f659d059713b316103b161298038225971beb3fa
matheus gabriel <matheuscoding@gmail.com>
2020-07-29 13:08:57 -0300

    changed content

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import Logo from '../../assets/img/logo.png';
 import { FooterBase } from './styles';
 
 function Footer() {
   return (
     <FooterBase>
       <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+        <img className="logo" src={Logo} alt="logo" />
       </a>
       <p>
-        Orgulhosamente criado durante a
+        Orgulhosamente criado por Matheus Gabriel durante a
         {' '}
-        <a href="https://www.alura.com.br/">
+        <a href="/">
           Imersão React da Alura
         </a>
       </p>
